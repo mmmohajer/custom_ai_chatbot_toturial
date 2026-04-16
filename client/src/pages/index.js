94b93cb5dc3e6d9ff70507278432ce82253a6100
mmmohajer70 <mmmohajer70@gmail.com>
2026-04-16 15:24:08 -0400

    Setup for day 1 is completed

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+const index = () => (
+  <div>
+    <h1>Welcome to the Custom Chatbot Application</h1>
+    <p>This is the home page. Use the navigation to explore the features.</p>
+  </div>
+);
+
+export default index;
